6091e3d9ea7c11afecf9cc2e3c7c30960591b18b
seby92 <seby922@gmail.com>
2023-06-07 13:16:08 +0300

    elso

---
+++ Hazi-06-07/Hazi-06-07/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SchoolTrackerV2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentNames = new List<string>();
+            var studentNotes = new List<int>();
+
+            bool nextStudent = true;
+
+            while (nextStudent)
+            {
+                Console.Write("Add meg a nevet: ");
+                string name = Console.ReadLine();
+                studentNames.Add(name);
+
+                Console.Write("Add meg a jegyet: ");
+                int note = int.Parse(Console.ReadLine());
+                studentNotes.Add(note);
+
+                Console.Write("Lesz uj diak? y/n: ");
+                nextStudent = Console.ReadLine() == "y";
+            }
+
+            for (int i = 0; i < studentNames.Count; i++)
+            {
+                Console.WriteLine($"Nev: {studentNames[i]}; Jegy: {studentNotes[i]}");
+            }
+
+        }
+    }
+}
